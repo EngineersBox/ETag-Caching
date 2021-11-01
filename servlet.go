c3354928f0ff4a5ce0cadad8bc68242a585cbdf7
EngineersBox <35655145+EngineersBox@users.noreply.github.com>
2021-11-01 13:34:49 +1100

    Added handling of `last-modified` and `if-modified-since` headers

--- servlet.go
+++ servlet.go
@@ -5,10 +5,23 @@ import (
 	"encoding/hex"
 	"io/ioutil"
 	"net/http"
+	"time"
 )
 
 const (
 	cacheContentFile = "./cachable.html";
+)
+
+var (
+	location, _ = time.LoadLocation("GMT");
+)
+
+func currentDateTime() string {
+	return time.Now().In(location).Format("Mon, 02 Jan 2006 15:04:05 GMT")
+}
+
+var (
+	lastModified = currentDateTime();
 )
 
 func generateDigest(value string) string {
@@ -24,17 +37,22 @@ func cache(w http.ResponseWriter, req *http.Request) {
 	}
 	currentDigest := generateDigest(string(fileContent));
 	headerDigest := req.Header.Get("if-none-match");
+	modifiedSince := req.Header.Get("if-modified-since")
+	w.Header().Add("ETag", currentDigest);
 	if len(headerDigest) == 0 {
-		w.Header().Add("ETag", currentDigest);
+		lastModified = currentDateTime()
+		w.Header().Add("last-modified", lastModified)
 		w.Write(fileContent);
 		return;
 	}
-	if headerDigest == currentDigest {
+	if headerDigest == currentDigest && modifiedSince == lastModified {
 		w.WriteHeader(http.StatusNotModified);
+		w.Header().Add("last-modified", lastModified)
 		w.Write([]byte{});
 		return;
 	}
-	w.Header().Add("ETag", currentDigest);
+	lastModified = currentDateTime()
+	w.Header().Add("last-modified", lastModified)
 	w.Write(fileContent);
 }
 
